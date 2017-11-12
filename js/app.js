9b4e2af1801c00233bc88e76fa1f1ef36a073f53
NatalyOC <noteroc@hotmail.com>
2017-11-12 01:01:23 -0500

    agregando funcionalidad por medio js y modificando html

--- js/app.js
+++ js/app.js
@@ -8,60 +8,94 @@
 //     // cells.addEventListener('click',reset)
    
 // };
-// var centinel=true;
-// function addXorO(){
 
-//     if(event.target.matches('td') && event.target.textContent===''){
-//         if(centinel){
-//             event.target.textContent='X';
-//         }
-//         else {
-//             event.target.textContent='O';
-//         }
-//         centinel=!centinel;
-//     }
+//Declaracion de variables
+var button = document.querySelector('.btnReset'); //Declaro variable que almacenara el primer elemento en el documento con class ="btnReset"
+var cells = document.querySelectorAll('.cell'); //Declaro variable que almacenara el una coleccion de elementos del documento con class ="cell"
+var arrTicTacToe= []; //array que almacenará los elementos del juego 
+var board = document.querySelector('.board-js'); //Declaro variable que almacenara el primer elemento en el documento con class ="board-js"
 
-// }
 
-var board = document.querySelector('.board-js');
+
+
+//Asignar a todos los elementos que tienen la clase board el evento click y la funcion "addXorO"
 board.addEventListener('click',addXorO);
 
 var centinel=true;
-function addXorO(){
+var count=0; //contador para los resultados
 
+
+function addXorO(){
+    var idCell=event.target.id;//variable que almacenara el id del elemento que desencadeno el evento
+    var positionCell=idCell[1]-1;
     if(event.target.matches('td') && event.target.textContent===''){
         if(centinel){
+
             event.target.textContent='X';
+            arrTicTacToe[positionCell]='X';
+            count++;
+            wiener('X');
         }
         else {
             event.target.textContent='O';
+            count++;
+            arrTicTacToe[positionCell]='O';
+            wiener('O');
         }
         centinel=!centinel;
+        console.log(arrTicTacToe,positionCell,idCell,count);
     }
+    if(count==9){
+        alert('empate'); 
+        window.location.reload()//La pagina se recarga por medio del metodo reload del objeto location 
+    }
+    
 
 }
 
-var button = document.querySelector('.btnReset');
-var cells = document.querySelectorAll('.cell');
+
+
+
+
+
+//Declaracion de Funciones
+
+
+//document.getElementsByClassName('cell').addEventListener('click',play)
+
+function wiener(letter){
+    //cualquier de estas 8 que se cumplen 
+    if(
+        (arrTicTacToe[0]==letter && arrTicTacToe[1]==letter && arrTicTacToe[2]==letter) ||
+        (arrTicTacToe[3]==letter && arrTicTacToe[4]==letter && arrTicTacToe[5]==letter) || 
+        (arrTicTacToe[6]==letter && arrTicTacToe[7]==letter && arrTicTacToe[8]==letter) || 
+        (arrTicTacToe[0]==letter && arrTicTacToe[3]==letter && arrTicTacToe[6]==letter) ||
+        (arrTicTacToe[1]==letter && arrTicTacToe[4]==letter && arrTicTacToe[7]==letter) || 
+        (arrTicTacToe[2]==letter && arrTicTacToe[5]==letter && arrTicTacToe[8]==letter) || 
+        (arrTicTacToe[0]==letter && arrTicTacToe[4]==letter && arrTicTacToe[8]==letter) || 
+        (arrTicTacToe[2]==letter && arrTicTacToe[4]==letter && arrTicTacToe[6]==letter)
+
+)
+    
+    {
+        alert('Jugador'+ letter+'GANA');
+        window.location.reload();
+    }
+}
+function play(event){
+    //mandame la alerta el id del elemento que desencadeno el evento especifico
+    alert(event.target.id);
+    var idCell=event.target.id;
+
+    //objeto global
+    var cell=event.target;
+    var cell
+
+}
 
 button.onclick=function(){
-   for(var i=0 ; i<cells.length;i++){
-     cells[i].textContent='';
-  }
-
-}
-
-
-/*function wiener(){
-var text=document.getElementById("row-1").value;
-// for(var row=1;row<=3;row++){
-//   var count= 0;
-//   for(var col=1;col<=3;col++){
-//    var text='';
-//    text=document.getElementById('table-board').rows[row].cells[col].value;
-//   }
-// }
-  alert(text);
-}*/
-
-
+    for(var i=0 ; i<cells.length;i++){
+      cells[i].textContent='';
+   }
+ 
+ }
